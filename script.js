d13489ba49d6e7a00ab355b1787d8b71d0276815
yukiecanatto <yukiecanatto@gmail.com>
2025-01-22 15:36:04 -0300

    adição função enviar e alert no arquivo script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-
+//Botão adicionar ao carrinho 'produtos.html'
 document.querySelectorAll('.botao-add-carrinho').forEach(button => {
     button.addEventListener('click', event => {
         event.preventDefault(); 
         alert('Item adicionado ao carrinho com sucesso!');
     });
 });
+
+//Botão enviar do forms 'contato.html'
+document.getElementById('enviarDados').addEventListener("click", (event) => {
+    event.preventDefault();
+    alert('WOOFF! WOOFF! Entraremos em contato');
+    
+    // Reseta o form
+    const form = document.querySelector("form");
+    form.reset();
+});
+
