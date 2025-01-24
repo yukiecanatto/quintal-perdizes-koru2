df2ba9631460f8f67c84e21a7b79548f90aeb844
yukiecanatto <yukiecanatto@gmail.com>
2025-01-24 18:21:52 -0300

    msg de erro quando campos vazios no form em 'contato'

--- script.js
+++ script.js
@@ -13,13 +13,54 @@ document.querySelectorAll('.botao-add-carrinho').forEach(button => {
 //Botão enviar do forms 'contato.html'
 document.getElementById('enviarDados').addEventListener("click", (event) => {
     event.preventDefault();
-    
-    // Exibe o modal de sucesso
-    const modal = new bootstrap.Modal(document.getElementById('addToCartModal'));
-    modal.show();
-    
-    // Reseta o form
-    const form = document.querySelector("form");
-    form.reset();
-});
 
+    // Campos de entrada
+    const nome = document.querySelector('input[name="nome"]');
+    const email = document.querySelector('input[name="email"]');
+    const mensagem = document.querySelector('textarea[name="mensagem"]');
+
+    // Onde vão as mensagens de erro
+    const nomeError = document.getElementById('nomeError');
+    const emailError = document.getElementById('emailError');
+    const mensagemError = document.getElementById('mensagemError');
+
+    // Limpa as mensagens de erro anteriores
+    nomeError.textContent = '';
+    emailError.textContent = '';
+    mensagemError.textContent = '';
+
+    // Validação dos campos
+    let isValid = true;
+
+    if (nome.value === "") {
+        nomeError.textContent = "Por favor, insira seu nome.";
+        isValid = false;
+    }
+
+    if (email.value === "") {
+        emailError.textContent = "Por favor, insira seu e-mail.";
+        isValid = false;
+    }
+
+    if (mensagem.value === "") {
+        mensagemError.textContent = "Por favor, preencha a mensagem.";
+        isValid = false;
+    }
+
+    // Se os campos estiverem todos preenchidos corretamente
+    if (isValid) {
+        // Exibe a mensagem de sucesso no modal
+        const modalMessage = document.getElementById('sendModalMessage');
+        const modalTitle = document.getElementById('sendModalLabel');
+        modalTitle.textContent = "WOOF! WOOF!";
+        modalMessage.textContent = "Entraremos em contato em breve!";
+
+        // Exibe o modal de sucesso
+        const successModal = new bootstrap.Modal(document.getElementById('sendModal'));
+        successModal.show();
+
+        // Reseta o formulário
+        const form = document.querySelector("form");
+        form.reset();
+    }
+});
