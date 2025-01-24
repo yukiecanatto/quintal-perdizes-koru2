cc352840444423276c8541beb526cbeb25b3428e
yukiecanatto <yukiecanatto@gmail.com>
2025-01-24 16:57:54 -0300

    popup add ao carrinho

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //Botão adicionar ao carrinho 'produtos.html'
 document.querySelectorAll('.botao-add-carrinho').forEach(button => {
     button.addEventListener('click', event => {
-        event.preventDefault(); 
-        alert('Item adicionado ao carrinho com sucesso!');
+        event.preventDefault();
+
+        // Exibe o modal de sucesso
+        const modal = new bootstrap.Modal(document.getElementById('addToCartModal'));
+        modal.show();
     });
 });
+
 
 //Botão enviar do forms 'contato.html'
 document.getElementById('enviarDados').addEventListener("click", (event) => {
